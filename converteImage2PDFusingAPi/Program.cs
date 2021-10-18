fb8379e128556732de9942448e24ed66b7d4f038
shane.gracias <shane.gracias@SYSLP559>
2021-10-18 17:54:39 +0530

    initial push

---
+++ converteImage2PDFusingAPi/Program.cs
@@ -0,0 +1,32 @@
+﻿using Aspose.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace converteImage2PDFusingAPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = "C:\\Users\\shane.gracias\\Documents\\" + "cat.jpg";
+
+            // Initialize new PDF document
+            Document doc = new Document();
+
+            // Add empty page in empty document
+            Page page = doc.Pages.Add();
+            Aspose.Pdf.Image image = new Aspose.Pdf.Image();
+            image.File = (path);
+
+            // Add image on a page
+            page.Paragraphs.Add(image);
+
+            // Save output PDF file
+            doc.Save("C:\\Users\\shane.gracias\\Documents\\" + "ImagetoPDF.pdf");
+        }
+    }
+}
--- convertingImage2PDF/Program.cs
+++ convertingImage2PDF/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using iTextSharp.text;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,24 @@ namespace convertingImage2PDF
     {
         static void Main(string[] args)
         {
+            
+           Document document = new Document();
+            using (var stream = new FileStream("test.pdf", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                iTextSharp.text.pdf.PdfWriter.GetInstance(document, stream);
+                document.Open();
+                using (var imageStream = new FileStream("pokemon.png", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    var image = Image.GetInstance(imageStream);
+                    document.Add(image);
+                }
+                document.Close();
+            }
+
+
+
         }
+
+
     }
 }
