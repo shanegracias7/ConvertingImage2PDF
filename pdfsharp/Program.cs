02620da65898d4145884ea0b0c2a8aac3ba24c98
shane.gracias <shane.gracias@SYSLP559>
2021-10-18 20:26:08 +0530

    -added open source library  (pdfsharp)

---
+++ pdfsharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+
+namespace pdfsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PdfDocument doc = new PdfDocument();
+
+            doc.Pages.Add(new PdfPage());
+
+            XGraphics xgr = XGraphics.FromPdfPage(doc.Pages[0]);
+
+            XImage img = XImage.FromFile("C:\\Users\\shane.gracias\\Documents\\cat.jpg");
+
+            xgr.DrawImage(img, 0, 0);
+
+            doc.Save("C:\\Users\\shane.gracias\\Documents\\rat.pdf");
+
+            doc.Close();
+        }
+    }
+}
